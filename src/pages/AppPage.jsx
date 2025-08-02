58a7c41a10852fb8d03132d5a906d7c865073ee5
abhinit1999 <krabhinit60@gmail.com>
2025-08-02 12:34:05 +0530

    added AI symbol to tryAPP page

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const AppPage = () => (
   <div className="min-h-screen p-8 bg-white center flex flex-col items-center">
-    <h1 className="text-3xl font-bold mb-4">📊Prompt2SQL</h1>
+    <h1 className="text-3xl font-bold mb-4">
+        {/* <img src="img.png" alt="Logo" width={100} height={250}  className="h-12 inline-block mr-2" /> */}
+       ✈️ Prompt2SQL</h1>
     <p className="text-gray-600">This is AI-powered tool that converts natural language questions into executable SQL queries. It’s designed to help non-technical users interact with databases using plain English, without needing to write SQL manually.</p>
   </div>
 );
